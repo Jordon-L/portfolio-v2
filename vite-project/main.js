ab72c45dc093e4972e0d61c87c1518c4e018f648
Jordon-L <jordonlee1412@gmail.com>
2022-09-08 23:03:40 -0700

    added more projects, moved about to home section

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,34 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
+const log = document.getElementById('log');
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+document.getElementById('form-contact').addEventListener("submit", function(evt) {
+  contactMe();
+  log.textContent = `Form Submitted!`;
+  name.value = "";
+  email.value = "";
+  message.value = "";
+  evt.preventDefault();
+}, true);
+
+function contactMe(){
+  const headers = new Headers()
+  headers.append("Content-Type", "application/json")
+  console.log(message.value)
+  const body = {
+    "name": name.value,
+    "email": email.value,
+    "message": message.value
+  }
+  
+  const options = {
+    method: "POST",
+    headers,
+    mode: "cors",
+    body: JSON.stringify(body),
+  }
+  
+  fetch("https://eof3acbu78ad7fd.m.pipedream.net", options)
+}
 
