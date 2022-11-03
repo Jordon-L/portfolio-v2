03e86a68ce7b2a76c49215303f1fd33a03617984
Jordon-L <jordonlee1412@gmail.com>
2022-11-02 20:50:35 -0700

    basic error check in form

--- main.js
+++ main.js
@@ -6,30 +6,35 @@ const email = document.getElementById('email');
 const message = document.getElementById('message');
 document.getElementById('form-contact').addEventListener("submit", function(evt) {
   contactMe();
-  log.textContent = `Form Submitted!`;
-  name.value = "";
-  email.value = "";
-  message.value = "";
   evt.preventDefault();
 }, true);
 
 function contactMe(){
   const headers = new Headers()
   headers.append("Content-Type", "application/json")
-  console.log(message.value)
-  const body = {
-    "name": name.value,
-    "email": email.value,
-    "message": message.value
+  if(name.value != '' && email.value != '' && message.value != ''){
+    const body = {
+      "name": name.value,
+      "email": email.value,
+      "message": message.value
+    }
+    
+    const options = {
+      method: "POST",
+      headers,
+      mode: "cors",
+      body: JSON.stringify(body),
+    }
+    
+    fetch("https://eof3acbu78ad7fd.m.pipedream.net", options)
+    log.textContent = `Form Submitted!`;
+    name.value = "";
+    email.value = "";
+    message.value = "";
   }
-  
-  const options = {
-    method: "POST",
-    headers,
-    mode: "cors",
-    body: JSON.stringify(body),
+  else{
+    console.log('empty')
+    log.textContent = `Please fill in form`;
   }
-  
-  fetch("https://eof3acbu78ad7fd.m.pipedream.net", options)
 }
 
