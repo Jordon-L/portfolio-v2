5f7374ef6250f16d4703471a16367b67608f7818
Jordon-L <jordonlee1412@gmail.com>
2023-03-23 20:12:01 -0700

    remove vite plugin

--- vite-config.js
+++ vite-config.js
@@ -1,13 +1,4 @@
 // vite.config.js
-import { VitePluginRadar } from 'vite-plugin-radar'
-
 export default {
-  plugins: [
-    VitePluginRadar({
-      // Google Analytics tag injection
-      analytics: {
-        id: 'G-MBT6WXE2PN',
-      },
-    })
-  ],
+  // config options
 }
